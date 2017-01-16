b866292af1df128b37b91565ec70e30e2e0130e5
Rafael Minguet <rminguet@gmail.com>
2017-01-16 22:20:49 +0100

    Routing - dashboard and heroes list

---
+++ app/dashboard.component.ts
@@ -0,0 +1,21 @@
+import {Component, OnInit} from '@angular/core';
+
+import {Hero} from "./hero";
+import {HeroService} from "./hero.service";
+
+// moduleId : module.id -> for relative paths in templateUrl
+@Component({
+    selector   : 'my-dashboard',
+    moduleId   : module.id,
+    templateUrl: 'dashboard.component.html',
+})
+export class DashboardComponent {
+    heroes:Hero[] = [];
+
+    constructor(private heroService:HeroService) {
+    }
+
+    ngOnInit():void {
+        this.heroService.getHeroes().then(heroes => this.heroes = heroes.slice(1, 5));
+    }
+}
